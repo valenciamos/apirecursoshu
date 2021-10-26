34509eb2b2ac87f240c801425fd1b8d2c929ce2f
eydymosquera <eydymosval05@hotmail.com>
2021-10-26 16:24:55 -0500

    listo

--- src/main/java/co/usa/recursosh/recursosh/model/Reservas.java
+++ src/main/java/co/usa/recursosh/recursosh/model/Reservas.java
@@ -16,7 +16,7 @@ public class Reservas implements Serializable{
     private Date startDate;
     private Date devolutionDate;
     private String status="created";
-    private String score; 
+     
 
     @ManyToOne
     @JoinColumn(name= "id")
@@ -27,65 +27,50 @@ public class Reservas implements Serializable{
     @JoinColumn(name= "clientId")
     @JsonIgnoreProperties({"reservations","messages"})
     private Clientes client;
-
+    private String score;
     public Integer getIdReservation() {
         return idReservation;
     }
-
     public void setIdReservation(Integer idReservation) {
         this.idReservation = idReservation;
     }
-
     public Date getStartDate() {
         return startDate;
     }
-
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
-
     public Date getDevolutionDate() {
         return devolutionDate;
     }
-
     public void setDevolutionDate(Date devolutionDate) {
         this.devolutionDate = devolutionDate;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
+    public Audience getAudience() {
+        return audience;
+    }
+    public void setAudience(Audience audience) {
+        this.audience = audience;
+    }
+    public Clientes getClient() {
+        return client;
+    }
+    public void setClient(Clientes client) {
+        this.client = client;
+    }
     public String getScore() {
         return score;
     }
-
     public void setScore(String score) {
         this.score = score;
     }
 
-    public Audience getAudience() {
-        return audience;
-    }
-
-    public void setAudience(Audience audience) {
-        this.audience = audience;
-    }
-
-    public Clientes getClient() {
-        return client;
-    }
-
-    public void setClient(Clientes client) {
-        this.client = client;
-    }
-
+}
 
     
-
-
-}
